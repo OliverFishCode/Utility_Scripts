7ca37b7db4d5d0b1ef7610052f189760ad941032
Devon C Oliver <dolive3@siu.edu>
2018-04-23 18:44:03 -0500

    data nab

--- categorizer.py
+++ categorizer.py
@@ -6,16 +6,19 @@ Created on Mon Apr 23 15:51:21 2018
 """
 
 import pandas as pd
-
-data = {'0':[1,2,3,4,5],'1':[2,2,3,2,3]}
+import numpy as np
+data = np.array(([1,2,3,4,5,np.NaN,np.NaN],[2,2,3,2,3,np.NaN,np.NaN]))
+data = np.transpose(data)
 df1 = pd.DataFrame(data, columns = ['0','1'])
-data2 = {'0':[1,0,1,0,1],'1':[1,1,1,1,1]}
-df2 = pd.DataFrame(data2, columns = ['0','1'])
+data2 = np.array(([1,1,.5,0,0,np.NaN,np.NaN],[1,0,1,0,1,np.NaN,np.NaN]))
+data2 = np.transpose(data2)
+df2 = pd.DataFrame(data, columns = ['0','1'])
 df3 = pd.DataFrame()
+
 def catsr(x):
     if x <= 2:
         return "trib"
     if x > 2 and x <= 5:
         return "ohio"
 for col in df1.columns:
-    df1[col] = df1[col].apply(lambda x: catsr(x))
+    df3[col] = df1[col].apply(lambda x: catsr(x))
