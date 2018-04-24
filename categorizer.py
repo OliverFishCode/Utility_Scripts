5a47ea9e947e1d6cc4628b5b18cb4ef1dfeccdbf
Devon C Oliver <dolive3@siu.edu>
2018-04-23 22:43:37 -0500

    value replacer for smooth file

--- categorizer.py
+++ categorizer.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 23 15:51:21 2018
-
-@author: olive
-"""
 
 import pandas as pd
 import numpy as np
-data = np.array(([1,2,3,4,5,np.NaN,np.NaN],[2,2,3,2,3,np.NaN,np.NaN]))
-data = np.transpose(data)
-df1 = pd.DataFrame(data, columns = ['0','1'])
+data = np.array(([1,2,3,4,5,5,np.NaN,np.NaN],[2,2,3,2,np.NaN,np.NaN,np.NaN,np.NaN],[4,4,2,5,1,np.NaN,np.NaN,np.NaN]))
+df1 = pd.DataFrame(data, columns = ['0','1','2','3','4','5','6','7'])
 data2 = np.array(([1,1,.5,0,0,np.NaN,np.NaN],[1,0,1,0,1,np.NaN,np.NaN]))
-data2 = np.transpose(data2)
-df2 = pd.DataFrame(data, columns = ['0','1'])
+df2 = pd.DataFrame(data2, columns = ['0','1','2','3','4','5','6'])
 df3 = pd.DataFrame()
 
 def catsr(x):
